9f4d0fad616a19cd30e0c0e5ac692e61f4edcc6b
Tom Macgregor <tomamacgregor@gmail.com>
2020-02-13 00:20:52 +0000

    Add new class for plotting

--- structure_factor_tools/structure_factor_tools_plot.py
+++ structure_factor_tools/structure_factor_tools_plot.py
@@ -2,3 +2,13 @@
 
 import numpy as np, os, pandas as pd
 from structure_factor_tools_calculate import StructureFactorSimulation
+
+class StructureFactorAnalyser():
+    def __init__(self):
+        self.z_values = []
+        self.structure_factors = StructureFactorSimulation.calculateLatticeStructureFactor()
+
+    def compareStructureFactorsPlot(self, l1,l2):
+        '''Measure difference in structure factor w.r.t. the z position of atoms in a lattice then plot the results. '''
+        lattice_a =
+            
