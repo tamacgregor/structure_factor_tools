47f7ef1d88796bf93e5a12edca1cce031bf35ec4
Tom Macgregor <tomamacgregor@gmail.com>
2020-02-13 18:56:47 +0000

    Fixes and updates to class definitions. Tested the z change plot.

--- structure_factor_tools/structure_factor_tools_plot.py
+++ structure_factor_tools/structure_factor_tools_plot.py
@@ -2,15 +2,14 @@
 
 import numpy as np, os, pandas as pd, pixstem.api as ps, scipy as sp
 import matplotlib.pylab as plt
-from structure_factor_tools.structure_factor_tools_calculate. import StructureFactorSimulation
 
 class StructureFactorAnalyser(StructureFactorSimulation):
 
-    def __init__(self,lattice_file):
+    def __init__(self,lattice_file, name, no_atoms):
         '''Constructor function'''
         pass
-        StructureFactorSimulation.__init__(self)
-        self.lattice_data = StructureFactorSimulation.getLatticeInfo(lattice_file)
+        StructureFactorSimulation.__init__(self, name, no_atoms)
+        self.lattice_data = StructureFactorSimulation.getLatticeInfo(self,lattice= lattice_file)
         self.z_values = np.arange(5)
         self.structure_factors = []
 
@@ -18,12 +17,15 @@ class StructureFactorAnalyser(StructureFactorSimulation):
         '''Measure structure factor w.r.t. the z position of atoms
         in a lattice then plot the results. '''
         self.z_values = np.arange(start,end,step)
-        sim = StructureFactorSimulation
-        for i in self.z_values:
-            new_positions = sim.changeCoordinates(self,row= atom, x = self.lattice_data.x,  y = self.lattice_data.y, z = i, show_changes = show_progress)
-            new_structure_factor = sim.calculateLatticeStructureFactor(self)
+        for i in range (0, len(self.z_values))import :
+            new_positions = self.changeCoordinates(self,row= atom,
+             x = self.lattice_data.x,  y = self.lattice_data.y, z = self.z_values[i],
+             show_changes = show_progress)
+            new_structure_factor = self.calculateLatticeStructureFactor(self)
             self.structure_factors.append(new_structure_factor)
             del(new_structure_factor)
         plt.plot(self.z_values, self.structure_factors)
-
+        plt.sxlabel('z Postion (Fraction of Unit Cell)')
+        plt.ylabel('|$F_{hkl}$| (m$^{-1}$)')
+        plt.savefig(self.name + '.png', dpi = 600)
         return self.z_values, self.structure_factors
